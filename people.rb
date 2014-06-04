532ebe60331260f6b935fd951f37972d12d112bd
theoandtheb <blue.apple@sympatico.ca>
2014-06-04 16:01:05 -0400

    Well, it seems as if instructors can teach, students can learn, but students cannot teach.  I wonder if instructors can learn?

--- people.rb
+++ people.rb
@@ -13,20 +13,22 @@ end
 
 class Student < Person
 	def learn
-		puts "I get it!"
+		p "I get it!"
 	end
 end
 
 class Instructor < Person
-	def learn
-		puts "Everything in Ruby is an Object"
+	def teach
+		p "Everything in Ruby is an Object"
 	end
 end
 
 chris = Instructor.new
 chris.name = "Chris"
 chris.hey
+chris.teach
 
 christina = Student.new
 christina.name = "Cristina"
 christina.hey
+christina.learn
